1acf161ee66348710d94e9640199e5b27eb4926e
Pamedg <pameladiazglez2507@gmail.com>
2025-04-28 11:43:20 -0600

    css popup

--- src/components/ImagePopup/imagePopup.jsx
+++ src/components/ImagePopup/imagePopup.jsx
@@ -1,21 +1,23 @@
 export default function ImagePopup(props) {
   const { card, onClose, name, link } = props;
-  if (!card) {
-    return null;
-  }
+  /* if (!card) {
+  // return null;
+  // }*/
 
   return (
-    <div className="popup popup__image">
-      <div className="popup__content popup__content_content_image">
-        <button
-          aria-label="Close modal"
-          className="popup__close"
-          type="button"
-          onClick={onClose}
-        />
-        <img src={link} alt={name} className="popup__image" />
-        <h3 className="popup__title">{name}</h3>
+    <>
+      <div className="popup popup__image">
+        <div className="popup__container">
+          <img src={link} alt={name} className="popup__image" />
+          <h4 className="popup__title">{name}</h4>
+          <button
+            aria-label="Close modal"
+            className="popup__close"
+            type="button"
+            onClick={onClose}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
